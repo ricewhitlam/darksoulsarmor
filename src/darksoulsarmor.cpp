dcd3edb52b9d1b75d73412b3874aa7b262f0ee15
ricewhitlam <chase.s.benoist@gmail.com>
2025-10-03 17:43:44 -0400

    same as prior

--- src/darksoulsarmor.cpp
+++ src/darksoulsarmor.cpp
@@ -219,7 +219,7 @@ struct armor_combo {
     double durability; double poise; double weight; double load; 
     bool operator<(const armor_combo& comparison) const
     {
-        double eps = 1.0e-10;
+        double eps = 1.0e-6;
         bool check1 = ((score+eps) > comparison.score);
         bool check2 = check1 && ((weight-eps) < comparison.weight);
         bool check3 = check2 && ((poise+eps) > comparison.poise);
